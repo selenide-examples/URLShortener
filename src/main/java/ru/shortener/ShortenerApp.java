9e54a5adcaea054bbcda3d8df34cf8689f33b7d0
Roman S.A <mail@safeone.ru>
2017-08-23 21:00:08 +0300

    initial

---
+++ src/main/java/ru/shortener/ShortenerApp.java
@@ -0,0 +1,11 @@
+package ru.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortenerApp.class, args);
+    }
+}
